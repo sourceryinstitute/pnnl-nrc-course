e399cbd80a4093dfc73d970d332fe0233bca7c88
Damian Rouson <damian@sourceryinstitute.org>
2020-07-21 09:49:53 -0700

    demonstrate implicit typing

---
+++ src/day-2/01-implicit-declaration.f90
@@ -0,0 +1,8 @@
+program main
+  !! without implict none, the first character of variables names 
+  !! determine their types: i-n -> integer; a-h, o-z -> real
+  i = 99
+  print *, jar, x
+    !! "*" indicates list-directed output
+    !! j is implicitly typed as an integer
+end program main
